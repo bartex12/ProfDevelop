2188709a6404a22517e7205594be2e3303596899
пк <barcats@inbox.ru>
2021-04-16 17:48:21 +0300

    немного изменил код в презентере

--- app/src/main/java/geekbrains/ru/translator/model/datasource/DataSourceLocal.kt
+++ app/src/main/java/geekbrains/ru/translator/model/datasource/DataSourceLocal.kt
@@ -3,8 +3,8 @@ package geekbrains.ru.translator.model.datasource
 import geekbrains.ru.translator.model.data.DataModel
 import io.reactivex.Observable
 
-class DataSourceLocal(private val remoteProvider: RoomDataBaseImplementation = RoomDataBaseImplementation()) :
+class DataSourceLocal(private val localProvider: RoomDataBaseImplementation = RoomDataBaseImplementation()) :
     DataSource<List<DataModel>> {
 
-    override fun getData(word: String): Observable<List<DataModel>> = remoteProvider.getData(word)
+    override fun getData(word: String): Observable<List<DataModel>> = localProvider.getData(word)
 }
